20c25efbc2924294f15ca03865f6b17ce4d2624a
JamesD777 <jcd160230@utdallas.edu>
2017-03-21 00:56:33 -0500

    Added delete and print

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import java.io.PrintWriter;
+
 /**
  *
  * @author jcd4912
@@ -21,9 +23,61 @@ public class LinkedList {
             head = new DoubleLinkNode(r,s,null,null);
             tail = head;
         }
-        else{
+        else if(tail.getRow() == r && tail.getSeat() == s){
             tail.setNext(new DoubleLinkNode(r,s,null,tail));
             setTail(tail.getNext());
         }
+        else{
+            DoubleLinkNode cur = head;
+            while(cur.getNext() != null){
+                if(cur.getRow() >= r){
+                    if(cur.getSeat() > s){
+                        DoubleLinkNode N = new DoubleLinkNode(r,s,cur,cur.getPrev());
+                        cur.getPrev().setNext(N);
+                        cur.setPrev(N);
+                        return;
+                    }
+                }
+                cur = cur.getNext();
+            }
+        }
     }
+    public void deleteNode(int r, int s){
+        DoubleLinkNode c = head;
+        while(c.getNext() != null){
+            if(c.getRow() == r){
+                if(c.getSeat() == s){
+                    c.getPrev().setNext(c.getNext());
+                    c.getNext().setPrev(c.getPrev());
+                    break;
+                }
+            }
+            c = c.getNext();
+        }
+    }
+    void print(PrintWriter out, DoubleLinkNode n)
+  	{
+            if(n == null)
+                return;
+            else{
+                out.print('.');
+                if(n.getNext().getRow() > n.getRow())
+                    out.println();
+                for(int i = n.getRow(); i < n.getNext().getRow()+1; i++){
+                    for(int j = n.getSeat(); j < n.getNext().getSeat(); j++)
+                            out.print('#');
+                    out.println();
+                }
+                if(n.getNext().getNext() != null)
+                    print(out, n.getNext());
+            }
+            out.print('.');
+            if(n.getNext().getRow() > n.getRow())
+                out.println();
+            for(int i = n.getRow(); i < n.getNext().getRow()+1; i++){
+                    for(int j = n.getSeat(); j < n.getNext().getSeat(); j++)
+                            out.print('#');
+                    out.println();
+                }
+            }
 }
